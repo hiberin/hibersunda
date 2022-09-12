1500284b3e49895e268fa59dc0cac50daa6e2167
Kamaludin Khoir <khoirkamaludin@gmail.com>
2022-09-12 23:04:58 +0700

    add new endpoint to get words by substring

--- main.go
+++ main.go
@@ -6,39 +6,85 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	// inisiasi router make gin
-	router := gin.Default()
-	// router get pikeun nyokot kabeh kata/kecap nu aya
-	router.GET("/kabehkecap", cokotKabehKecap)
+type Words struct {
+	Words []word `json:"words"`
+}
 
-	// ngajalankeun router dina port 8080
+type word struct {
+	Sorangan string `json:"sorangan"`
+	Batur    string `json:"batur"`
+	Loma     string `json:"loma"`
+	Bindo    string `json:"bindo"`
+}
+
+func main() {
+	// initiate router using gin
+	router := gin.Default()
+	router.GET("/undakusukbasa", getAllWords)
+	router.GET("/undakusukbasa/:substring", getWordsBySubstring)
+
+	// run the router on port 8080
 	router.Run("localhost: 8080")
 }
 
-// Fungsi pikeun nyokot kabeh kecap anu aya dina daptarKecap,json
-func cokotKabehKecap(c *gin.Context) {
-	jsonFile, err := os.Open("daptarKecap.json")
+/**
+* Function to get all the words
+ */
+func getAllWords(c *gin.Context) {
+	jsonFile, err := os.Open("./undakUsukBasa.json")
 
-	// lamun aya error
+	// if error happens
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	//* Lamun euweuh error
-	// Teundeun value tina jsonFile dina bentuk byte
+	// If no error
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+	// Save json content into byte
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-	// Inisiasi interface hasil
-	var hasil map[string]interface{}
+	// Initiate interface
+	var words Words
+	json.Unmarshal(byteValue, &words)
 
-	// parsing json kana variable hasil
-	json.Unmarshal([]byte(byteValue), &hasil)
+	// return json
+	c.IndentedJSON(http.StatusOK, words)
+}
 
-	// Pulangkeun eusi tina file json
-	c.IndentedJSON(http.StatusOK, hasil["daptarKecap"])
+/**
+* Function to get words containing suffix
+ */
+func getWordsBySubstring(c *gin.Context) {
+	substring := c.Param("substring")
+	jsonFile, err := os.Open("./undakUsukBasa.json")
+
+	// if error happens
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// If no error
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+	// Save json content into byte
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// Initiate interface
+	var words Words
+	json.Unmarshal(byteValue, &words)
+
+	foundWords := []interface{}{}
+	for i := 0; i < len(words.Words); i++ {
+		if strings.Contains(words.Words[i].Sorangan, substring) || strings.Contains(words.Words[i].Batur, substring) || strings.Contains(words.Words[i].Loma, substring) || strings.Contains(words.Words[i].Bindo, substring) {
+			foundWords = append(foundWords, words.Words[i])
+		}
+	}
+	c.IndentedJSON(http.StatusOK, foundWords)
+	return
 }
